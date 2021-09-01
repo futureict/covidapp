d7c773fa291a139789382a77e18dd0700947fa31
futureict <37749572+futureict@users.noreply.github.com>
2021-09-01 15:52:58 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react"
+import Covid from "./components/covid";
+
+const App = () => {
+ 
+    return (
+        <>
+            <h2>COVID - 19 TRACKER</h2>
+        </>
+    )
+};
+
+export default App;
